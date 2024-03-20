50e6a087f5062a024447fc0585019e288a5096ec
vmaso-vp <89546096+vmaso-vp@users.noreply.github.com>
2024-03-20 08:58:06 -0400

    build script split into new files

---
+++ Build_Deploy.ps1
@@ -0,0 +1,38 @@
+
+$Scripthome = $PSScriptRoot
+
+$moduleFolder = "$PSScriptRoot/psFilesCli"
+
+$CrecendoBuildersCompletePath = "$Scripthome/CrecendoCommandConfigsComplete"
+$CrecendoBuildersPath = "$Scripthome/CrescendoCommandConfigs"
+$CrescendoBuildersPath = "$Scripthome/CrescendoBuilders"
+
+. "$Scripthome\Build_Functions.ps1"
+
+$globalFlagsParameters = (Get-Content "$CrescendoBuildersPath/GlobalFlags.json" | ConvertFrom-Json).commands.parameters
+$StartAndEndFlagsParameters = (Get-Content "$CrescendoBuildersPath/StartAndEndFlags.json" | ConvertFrom-Json).commands.parameters
+
+$CrecendoBuilderFiles = Get-ChildItem $CrecendoBuildersPath -Recurse -File
+
+
+Get-ChildItem $CrecendoBuildersCompletePath | Where-Object {$_.FullName -match 'CrecendoCommandConfigsComplete(/|\\)CrecendoComplete_'} | Remove-Item -Force
+
+$CrecendoBuilderFiles | ForEach-Object {
+    $addStartAndEndFlags = $null
+
+    $psobjectofCresendoCommandConfig = (Get-Content $_.FullName | ConvertFrom-Json)
+
+    if ($psobjectofCresendoCommandConfig.AdditionalCrescendoBuilders -contains "StartAndEndFlags") {$addStartAndEndFlags = $true}
+
+
+    $commandConfig = Import-CommandConfiguration $_.FullName 
+    $commandConfig.Parameters += $globalFlagsParameters
+
+    if ($addStartAndEndFlags) {$commandConfig.Parameters += $StartAndEndFlagsParameters}
+
+
+    Export-CrescendoCommand -command $commandConfig -fileName "$CrecendoBuildersCompletePath/CrecendoComplete_$($_.Name)" -Force
+}
+
+Export-CrescendoModule -ConfigurationFile @((Get-ChildItem $CrecendoBuildersCompletePath).fullname) -ModuleName "$moduleFolder/psFilesCli.psm1" -Force
+
